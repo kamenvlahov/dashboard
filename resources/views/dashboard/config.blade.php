80ea1188e999081bbf30b0328c311c4594394929
kamenvlahov <kamen.sps@gmail.com>
2024-10-08 16:45:47 +0300

    Add back button clear unused wellcome

--- resources/views/dashboard/config.blade.php
+++ resources/views/dashboard/config.blade.php
@@ -3,11 +3,11 @@
 @section('content')
 <div class="container">
     <h1 class="text-center mb-4">Edit Dashboard Cell</h1>
-    
+
     <form action="{{ route('dashboard-cell.update', $cell->id) }}" method="POST">
         @csrf
         @method('PATCH') <!-- Use PATCH for partial updates -->
-        
+
         <div class="mb-3">
             <label for="title" class="form-label">Title</label>
             <input type="text" name="title" id="url" class="form-control" value="{{ old('title', $cell->title) }}">
@@ -22,14 +22,15 @@
             <select name="color_id" id="color" class="form-select">
                 <option value="">Select a color</option>
                 @foreach($colors as $color)
-                    <option value="{{ $color->id }}" {{ $color->id == $cell->color_id ? 'selected' : '' }}>
-                        {{ $color->color_name }}
-                    </option>
+                <option value="{{ $color->id }}" {{ $color->id == $cell->color_id ? 'selected' : '' }}>
+                    {{ $color->color_name }}
+                </option>
                 @endforeach
             </select>
         </div>
 
         <button type="submit" class="btn btn-primary">Update Cell</button>
+        <a href="{{ route('dashboard.index') }}" class="btn btn-secondary ms-2">Back</a>
     </form>
 </div>
 @endsection
